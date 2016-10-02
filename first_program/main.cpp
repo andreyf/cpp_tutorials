637254a6d8510a31471e0cb9efab70b620d08678
Andrey Fomin <andreyfomin82@gmail.com>
2016-10-02 20:07:59 +0300

    var_program

--- first_program/main.cpp
+++ first_program/main.cpp
@@ -3,6 +3,9 @@
 using namespace std;
 
 int main() {
-    cout << "Hello, World!" << endl;
-    return 0;
+    cout << "Введите число:"; // Выводми текст
+    int peremennaya; // Мы объявляем переменную типа Int
+    cin >> peremennaya; // Считываем введенное число в переменную "peremennaya"
+    cout << "Вывод числа " << peremennaya << "\n"; // Отображаем содержимое переменной
+    system("pause"); // Ожидаем нажатие клавиши
 }
