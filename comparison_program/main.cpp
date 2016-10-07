d5e38e1435fa8f65eaf088e9eb098f79fbaaf417
Andrey Fomin <andreyfomin82@gmail.com>
2016-10-07 16:38:40 +0300

    comparison_program

---
+++ comparison_program/main.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int perem1 = 4; // присваиваем perem1 4
+    int perem2 = 6; // присваиваем perem2 6
+    string text1 = "abc"; // присваиваем text1 "abc"
+    string text2 = "abc"; // присваиваем text2 "abc"
+
+    if(perem1 <= perem2) {
+        cout << "Переменная 2 больше";
+    }
+
+    if(tekst1 == tekst2) {
+        cout << "Текст 1 такой же, как текст 2";
+    }
+
+    return 0;
+}
