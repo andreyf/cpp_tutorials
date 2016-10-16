edeabe9a6f3c8eec8e96725e096ca0de969581c4
Andrey Fomin <andreyfomin82@gmail.com>
2016-10-16 12:48:31 +0300

    if_program

--- if_program/main.cpp
+++ if_program/main.cpp
@@ -4,5 +4,24 @@ using namespace std;
 
 int main() {
 
+    bool pravda = true;
+    int number1 = 1, number2 = 4;
+
+    if(pravda) cout << "Правда" << endl;
+
+    if(!pravda) {
+        cout << "Это не будет отображаться" << endl;
+    }
+    else {
+        cout << "А это будет ..." << endl;
+    }
+
+    if(number2 < number1) {
+        cout << "Это не будет отображаться" << endl;
+    }
+    else if(pravda) {
+        cout << "А это будет ..." << endl;
+    }
+
     return 0;
 }
